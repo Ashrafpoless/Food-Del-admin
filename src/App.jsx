b12f26456acc921308aa5ce42f220bcef85eee5e
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-02-13 00:41:48 +0100

    layout-cpmponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+// react router dom
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
+// components
+import Layout from './components/Layout/Layout'
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Add from './pages/Add/Add';
+import Orders from './pages/Orders/Orders';
+import List from './pages/List/List';
+//import Home from './pages/Home/Home.JSX';
 
 function App() {
   
+  
+  const Router = createBrowserRouter([
+    { path:'/', element:<Layout/>, errorElement: <ErrorPage/>, children:[
+       // {index:true , element :<Home/>},
+        {path:'/add', element: <Add/> },
+        {path:'/list', element: <List/>},
+        {path:'/orders', element: <Orders/>}
+
+    ]}
+])
 
   return (
     <>
-      <div>
-      
+      <div >
+        <RouterProvider router={Router}/>
       </div>
-      
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+
+//import PropTypes from 'prop-types'
+
+// assets
+import { assets } from '../../assets/assets';
+
+
+// components
+import './Navbar.css';
+
+const Navbar = () => {
+  
+  return (
+    <div className='navbar'>
+        <img className='logo' src={assets.logo} alt="" />
+        <img className='profile' src={assets.profile_image} alt="" />
+    </div>
+  )
+}
+
+Navbar.propTypes = {}
+
+export default Navbar
